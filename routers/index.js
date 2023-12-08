a18b8c60cfe541fc32edf2941b3d33c80851dccf
Quan <hoangminhquan123321@gmail.com>
2023-12-08 14:39:21 +0700

    Payment , deploy

--- routers/index.js
+++ routers/index.js
@@ -9,6 +9,7 @@ const uploadRoute = require('./upload.router');
 const addressRoute = require('./address.router');
 const postRoute = require('./post.router');
 const commentRoute = require('./comment.router');
+const paymentRoute = require('./payment.router');
 
 const appRoute = express();
 
@@ -22,5 +23,6 @@ appRoute.use('/upload', uploadRoute);
 appRoute.use('/address', addressRoute);
 appRoute.use('/post', postRoute);
 appRoute.use('/comment', commentRoute);
+appRoute.use('/payment', paymentRoute);
 
 module.exports = appRoute;
