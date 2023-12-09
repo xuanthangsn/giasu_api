a9a13670d5f1b89f51b607270c999adbd3d751ec
Quan <hoangminhquan123321@gmail.com>
2023-12-09 12:36:11 +0700

    DB Hosting

--- models/index.js
+++ models/index.js
@@ -5,13 +5,21 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-//const env = process.env.NODE_ENV || 'development';
-//const config = require(__dirname + '/../config/config.json')[env];
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-	dialect: 'postgres',
-});
+let sequelize;
+if (config.use_env_variable) {
+	sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+	sequelize = new Sequelize(
+		config.database,
+		config.username,
+		config.password,
+		config
+	);
+}
 sequelize
 	.authenticate()
 	.then(() => {
